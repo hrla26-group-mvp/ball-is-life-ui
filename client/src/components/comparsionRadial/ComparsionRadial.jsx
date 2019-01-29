2978d6deca45d2665356ba0b75d87985e5dd77c7
JY <JY@MacBook-Pro.local>
2019-01-29 14:58:57 -0800

    init svg and created circle for radar chart

--- client/src/components/comparsionRadial/ComparsionRadial.jsx
+++ client/src/components/comparsionRadial/ComparsionRadial.jsx
@@ -1,20 +1,55 @@
 import React from 'react';
 import axios from 'axios';
 
+const chartSize = 450;
+	const numberOfScales = 4;
+	const middleOfChart = (chartSize / 2).toFixed(4);
+	const scale = value => (
+		<circle
+			key={`scale-${value}`}
+			cx={0}
+			cy={0}
+			r={((value / numberOfScales) * chartSize) / 2}
+			fill="#FAFAFA"
+			stroke="#999"
+			strokeWidth="0.2"
+		/>
+	);
+
 class ComparisonRadial extends React.Component {
-    constructor(props){
-        super(props);
-    }
-    componentDidMount(){
-        axios.get('http://localhost:3000/api/stats')
-        .then(data => {
-            console.log(data)
-        })
-        .catch(err => console.log(err));
-    }
-    render(){
+  constructor(props){
+		super(props);
+			this.state={
+				data: ''
+			}
+	}
+	
+	componentDidMount(){
+			axios.get('http://localhost:3000/api/stats')
+			.then(data => {
+					this.setState({
+							data: data.players
+					})
+			})
+			.catch(err => console.log(err));
+	}
+  render(){
+		const groups = [];
+		const scales = [];
+		for (let i = numberOfScales; i > 0; i--) {
+			scales.push(scale(i));
+		}
+		groups.push(<g key={`scales`}>{scales}</g>);
         return (
-            <div></div>
+					<svg
+					version="1"
+					xmlns="http://www.w3.org/2000/svg"
+					width={chartSize}
+					height={chartSize}
+					viewBox={`0 0 ${chartSize} ${chartSize}`}
+				>
+					<g transform={`translate(${middleOfChart},${middleOfChart})`}>{groups}</g>
+				</svg>
         )
     }
 }
