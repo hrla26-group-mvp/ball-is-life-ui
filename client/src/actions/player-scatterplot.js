fdf18bba0af6fbad80159fe17e8fb36f1e4b8844
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-01-29 11:23:47 -0800

    [prog] Set up redux skeleton for scatterplot component

--- client/src/actions/player-scatterplot.js
+++ client/src/actions/player-scatterplot.js
@@ -1,4 +1,6 @@
 // ACTION CREATOR FUNCTIONS FOR PLAYER-SCATTERPLOT VIEW
+// these are functions that return action objects
+// these will be used by the functions inside mapDispatchToProps
 export default {
   changePlayers: players => ({ type: 'CHANGE_PLAYERS', players }),
 };
---
+++ client/src/containers/ScatterplotViewContainer.jsx
@@ -0,0 +1,18 @@
+import { connect } from 'react-redux';
+import ScatterplotView from '../components/player-scatterplot/ScatterplotView';
+
+// this provides whatever is inside the container with state properties as props
+// this replaces passing down this.state.XYZ as a prop
+const mapStateToProps = {
+  // STATE PROPS HERE
+};
+
+// this provides whatever is inside the container with fns that update state by dispatching actions
+// this replaces passing down functions ( e.g., (XYZ) => { this.setState(XYZ); } ) as a prop
+const mapDispatchToProps = {
+  // DISPATCH FNS HERE
+};
+
+const ScatterplotViewContainer = connect(mapStateToProps, mapDispatchToProps)(ScatterplotView);
+
+export default ScatterplotViewContainer;
