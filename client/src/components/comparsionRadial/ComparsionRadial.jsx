202a6b6c6603bfb2ba9f953bee86e2de96ceba28
JY <JY@MacBook-Pro.local>
2019-01-29 16:52:53 -0800

    refactor for eslint

--- client/src/components/comparsionRadial/ComparsionRadial.jsx
+++ client/src/components/comparsionRadial/ComparsionRadial.jsx
@@ -2,57 +2,56 @@ import React from 'react';
 import axios from 'axios';
 
 const chartSize = 450;
-	const numberOfScales = 4;
-	const middleOfChart = (chartSize / 2).toFixed(4);
-	const scale = value => (
-		<circle
-			key={`scale-${value}`}
-			cx={0}
-			cy={0}
-			r={((value / numberOfScales) * chartSize) / 2}
-			fill="#FAFAFA"
-			stroke="#999"
-			strokeWidth="0.2"
-		/>
-	);
+const numberOfScales = 4;
+const middleOfChart = (chartSize / 2).toFixed(4);
+const scale = value => (
+  <circle // setting radius and size of circle
+    key={`scale-${value}`}
+    cx={0}
+    cy={0}
+    r={((value / numberOfScales) * chartSize) / 2}
+    fill="#FAFAFA"
+    stroke="#999"
+    strokeWidth="0.2"
+  />
+);
+const data = [{two_points_pct: 0.5, three_points_pct: 0.388, points_in_paint_pct: 0.541, free_throws_pct: 0.604, field_goals_pct: 0.452}, 
+{two_points_pct: 0.5, three_points_pct: 0.388, points_in_paint_pct: 0.541, free_throws_pct: 0.604, field_goals_pct: 0.452}]
 
+
+const captions = Object.keys(data[0]);
+const columns = captions.map((key, i, all) => {
+  return {
+    key,
+    angle: (Math.PI * 2 * i) / all.length,
+  };
+});
+groups.push(<g key={`groups}`}>{data.map(shape(columns))}</g>);
 class ComparisonRadial extends React.Component {
-  constructor(props){
-		super(props);
-			this.state={
-				data: ''
-			}
-	}
-	
-	componentDidMount(){
-			axios.get('http://localhost:3000/api/stats')
-			.then(data => {
-					this.setState({
-							data: data.players
-					})
-			})
-			.catch(err => console.log(err));
-	}
-  render(){
-		const groups = [];
-		const scales = [];
-		for (let i = numberOfScales; i > 0; i--) {
-			scales.push(scale(i));
-		}
-		groups.push(<g key={`scales`}>{scales}</g>);
-        return (
-					<svg
-					version="1"
-					xmlns="http://www.w3.org/2000/svg"
-					width={chartSize}
-					height={chartSize}
-					viewBox={`0 0 ${chartSize} ${chartSize}`}
-				>
-					<g transform={`translate(${middleOfChart},${middleOfChart})`}>{groups}</g>
-				</svg>
-        )
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() { // start with empty svg and translate at end to move circle in center of svg
+    const groups = [];
+    const scales = [];
+    for (let i = numberOfScales; i > 0; i--) {
+      scales.push(scale(i));
     }
+    groups.push(<g key={`scales`}>{scales}</g>);
+    return (
+      <svg
+        version="1"
+        xmlns="http://www.w3.org/2000/svg"
+        width={chartSize}
+        height={chartSize}
+        viewBox={`0 0 ${chartSize} ${chartSize}`}
+      >
+        <g transform={`translate(${middleOfChart},${middleOfChart})`}>{groups}</g>
+      </svg>
+    );
+  }
 }
 
 export default ComparisonRadial;
-
