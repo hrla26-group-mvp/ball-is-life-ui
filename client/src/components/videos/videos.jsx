03720326908cb6d8c3fdfdc9e6af30fc36666fd0
Daniel Lin <daniellin215@gmail.com>
2019-01-31 11:37:24 -0800

    [prog]start video component

---
+++ client/src/components/videos/videos.jsx
@@ -0,0 +1,15 @@
+import React from 'react;
+import PropTypes from 'prop-types';
+import styles from '../../styles/videos';
+
+export default class Videos extends React.Component {
+  static PropTypes = {
+    videos: PropTypes.array.isRequired,
+    
+  };
+  render() {
+    return (
+
+    )
+  }
+}
