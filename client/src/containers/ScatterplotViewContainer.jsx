51e99297e4d3f533e825783a4254af956df2ec71
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-01-31 11:24:53 -0800

    [prog] App updates scatterplot data on initial fetch, set up scatterplot skeleton

--- client/src/containers/ScatterplotViewContainer.jsx
+++ client/src/containers/ScatterplotViewContainer.jsx
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { ScatterplotView } from '../components/scatterplot/ScatterplotView';
-import { changeScatterplotData } from '../actions/Scatterplot';
+import { updateScatterplotData } from '../actions/Scatterplot';
 
 const mapStateToProps = state => ({
   scatterplotData: state.scatterplotData,
@@ -8,9 +8,7 @@ const mapStateToProps = state => ({
 });
 
 const mapDispatchToProps = dispatch => ({
-  changeScatterplotData: (scatterplotData) => {
-    dispatch(changeScatterplotData(scatterplotData));
-  },
+  updateScatterplotData: (scatterplotData) => { dispatch(updateScatterplotData(scatterplotData)); },
 });
 
 const ScatterplotViewContainer = connect(mapStateToProps, mapDispatchToProps)(ScatterplotView);
