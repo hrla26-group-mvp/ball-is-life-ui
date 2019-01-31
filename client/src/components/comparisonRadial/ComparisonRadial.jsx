e331962808377430cecf2d1e986f7b3e95ca371e
JY <JY@MacBook-Pro.local>
2019-01-31 07:34:32 -0800

    [prod] adjusted format in comparison comp

--- client/src/components/comparisonRadial/ComparisonRadial.jsx
+++ client/src/components/comparisonRadial/ComparisonRadial.jsx
@@ -8,30 +8,9 @@ class ComparisonRadial extends Component {
   componentDidMount() {
     this.drawChart();
   }
-  const data1 = props.playerData[0];
-  const data2 = props.playerData[1];
-  if (data1) { // refactor to handle async
-    const arr1 = [
-      { 'assists per game': data1.assists_per_game },
-      { 'blocks per game': data1.blocks_per_game },
-      { 'field goals per game': data1.field_goals_made_per_game },
-      { 'three points per game': data1.three_points_made_per_game },
-      { 'two points per game': data1.two_points_made_per_game },
-      { 'rebounds per game': data1.rebounds_per_game },
-      { 'freethrows per game': data1.free_throws_made_per_game },
-    ];
-    const arr2 = [
-      { 'assists per game': data2.assists_per_game },
-      { 'blocks per game': data2.blocks_per_game },
-      { 'field goals per game': data2.field_goals_made_per_game },
-      { 'three points per game': data2.three_points_made_per_game },
-      { 'two points per game': data2.two_points_made_per_game },
-      { 'rebounds per game': data2.rebounds_per_game },
-      { 'freethrows per game': data2.free_throws_made_per_game },
-    ];
-  }
+  
 
-  drawChart() {
+  drawChart(){
     const data = [12, 5, 6, 6, 9, 10];
     
     const svg = d3.select("body")
@@ -56,6 +35,28 @@ class ComparisonRadial extends Component {
   //   playerTwo: PropTypes.object,
   // };      
   render(){
+  const data1 = props.playerData[0];
+  const data2 = props.playerData[1];
+  if (data1 && data2){ // refactor to handle async
+    const arr1 = [
+      { 'name': 'assists per game', 'data': data1.assists_per_game },
+      { 'name': 'blocks per game', 'data': data1.blocks_per_game },
+      { 'name': 'field goals per game', 'data': data1.field_goals_made_per_game },
+      { 'name': 'three points per game', 'data': data1.three_points_made_per_game },
+      { 'name': 'two points per game', 'data': data1.two_points_made_per_game },
+      { 'name': 'rebounds per game', 'data': data1.rebounds_per_game },
+      { 'name': 'freethrows per game', 'data': data1.free_throws_made_per_game },
+    ];
+    const arr2 = [
+      { 'name': 'assists per game', 'data': data2.assists_per_game },
+      { 'name': 'blocks per game', 'data': data2.blocks_per_game },
+      { 'name': 'field goals per game', 'data': data2.field_goals_made_per_game },
+      { 'name': 'three points per game', 'data': data2.three_points_made_per_game },
+      { 'name': 'two points per game', 'data': data2.two_points_made_per_game },
+      { 'name': 'rebounds per game', 'data': data2.rebounds_per_game },
+      { 'name': 'freethrows per game', 'data': data2.free_throws_made_per_game },
+    ];
+  };
     return (
       <div></div>
     )
