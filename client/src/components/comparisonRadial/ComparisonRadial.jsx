059c900884dcd563df95cefd0e133e602f875899
JY <JY@MacBook-Pro.local>
2019-01-31 07:49:43 -0800

    [refactor] adjusting bar chart set up

--- client/src/components/comparisonRadial/ComparisonRadial.jsx
+++ client/src/components/comparisonRadial/ComparisonRadial.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import d3 from 'd3';
 
-class ComparisonRadial extends Component { 
+class ComparisonRadial extends React.Component { 
     constructor(props){
       super(props);
+      const data1 = props.playerData[0];
+      const data2 = props.playerData[1];   
+      if (data1 && data2){ // refactor to handle async
+      const arr1 = [ 
+        { 'name': 'assists per game', 'data': data1.assists_per_game },
+        { 'name': 'blocks per game', 'data': data1.blocks_per_game },
+        { 'name': 'field goals per game', 'data': data1.field_goals_made_per_game },
+        { 'name': 'three points per game', 'data': data1.three_points_made_per_game },
+        { 'name': 'two points per game', 'data': data1.two_points_made_per_game },
+        { 'name': 'rebounds per game', 'data': data1.rebounds_per_game },
+        { 'name': 'freethrows per game', 'data': data1.free_throws_made_per_game },
+      ];
+      const arr2 = [
+        { 'name': 'assists per game', 'data': data2.assists_per_game },
+        { 'name': 'blocks per game', 'data': data2.blocks_per_game },
+        { 'name': 'field goals per game', 'data': data2.field_goals_made_per_game },
+        { 'name': 'three points per game', 'data': data2.three_points_made_per_game },
+        { 'name': 'two points per game', 'data': data2.two_points_made_per_game },
+        { 'name': 'rebounds per game', 'data': data2.rebounds_per_game },
+        { 'name': 'freethrows per game', 'data': data2.free_throws_made_per_game },
+      ];
+      console.log(arr1,' lkadjf')
+    };
     }
   componentDidMount() {
     this.drawChart();
@@ -11,8 +35,7 @@ class ComparisonRadial extends Component {
   
 
   drawChart(){
-    const data = [12, 5, 6, 6, 9, 10];
-    
+
     const svg = d3.select("body")
     .append("svg")
     .attr("width", w)
@@ -24,9 +47,9 @@ class ComparisonRadial extends Component {
       .enter()
       .append("rect")
       .attr("x", (d, i) => i * 70)
-      .attr("y", (d, i) => h - 10 * d)
+      .attr("y", (d, i) => h - 10 * d.data)
       .attr("width", 65)
-      .attr("height", (d, i) => d * 10)
+      .attr("height", (d, i) => d.data * 10)
       .attr("fill", "green")
   };
 
@@ -35,30 +58,9 @@ class ComparisonRadial extends Component {
   //   playerTwo: PropTypes.object,
   // };      
   render(){
-  const data1 = props.playerData[0];
-  const data2 = props.playerData[1];
-  if (data1 && data2){ // refactor to handle async
-    const arr1 = [
-      { 'name': 'assists per game', 'data': data1.assists_per_game },
-      { 'name': 'blocks per game', 'data': data1.blocks_per_game },
-      { 'name': 'field goals per game', 'data': data1.field_goals_made_per_game },
-      { 'name': 'three points per game', 'data': data1.three_points_made_per_game },
-      { 'name': 'two points per game', 'data': data1.two_points_made_per_game },
-      { 'name': 'rebounds per game', 'data': data1.rebounds_per_game },
-      { 'name': 'freethrows per game', 'data': data1.free_throws_made_per_game },
-    ];
-    const arr2 = [
-      { 'name': 'assists per game', 'data': data2.assists_per_game },
-      { 'name': 'blocks per game', 'data': data2.blocks_per_game },
-      { 'name': 'field goals per game', 'data': data2.field_goals_made_per_game },
-      { 'name': 'three points per game', 'data': data2.three_points_made_per_game },
-      { 'name': 'two points per game', 'data': data2.two_points_made_per_game },
-      { 'name': 'rebounds per game', 'data': data2.rebounds_per_game },
-      { 'name': 'freethrows per game', 'data': data2.free_throws_made_per_game },
-    ];
-  };
+  
     return (
-      <div></div>
+      <div id={"#" + root}></div>
     )
   }
 };
