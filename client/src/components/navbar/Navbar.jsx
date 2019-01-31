03ac1b3747e2c3aaf9cb930d6048b5997aa51970
Daniel Lin <daniellin215@gmail.com>
2019-01-30 17:14:24 -0800

    [prog]updating navbar component, adding titles

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -4,6 +4,12 @@ const Navbar = props => (
   <div id="navbar">
     <h1>Ball is Life</h1>
     <span>&#9776;</span>
+    <div>
+      Scatterplot
+    </div>
+    <div>
+      Compare Players
+    </div>
   </div>
 );
 
