769a26bed0944d8fd230ed7b78df340ef2dbc726
JY <JY@MacBook-Pro.local>
2019-01-31 16:10:43 -0800

    [prog] added axes and axis

--- client/src/components/comparisonRadial/ComparisonRadial.jsx
+++ client/src/components/comparisonRadial/ComparisonRadial.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import { select, scaleBand, scaleLinear } from 'd3';
-import styles from '../../styles/comparison/Comparison.css';
+// import PropTypes from 'prop-types';
+import Chart from './Chart.jsx';
+// import { select, scaleBand, scaleLinear } from 'd3';
+// import styles from '../../styles/comparison/Comparison.css';
 
 class ComparisonRadial extends React.Component { 
     constructor(props){
@@ -29,120 +30,85 @@ class ComparisonRadial extends React.Component {
       //   { 'name': 'freethrows per game', 'data': data2.free_throws_made_per_game },
       // ];
     }
-  componentDidMount() {
-    this.drawChart();
-  }
-  plot(chart, width, height){
-    const xScale = scaleBand()
-      .domain(data.map(d => d.name))
-      .padding(0.3)
-      .range([0, w]);
+  // componentDidMount() {
+  //   this.drawChart();
+  // }
+  
 
-    const yScale = scaleLinear()
-      .domain([0, 100])
-      .range([0, h]);
-    const colorScale = d3.scaleOrdinal(d3.schemaCategory20);
+  // drawChart(){
+  //   const data = [ 
+  //     { 'name': 'assists per game', 'data': 2 },
+  //     { 'name': 'blocks per game', 'data': 3.4 },
+  //     { 'name': 'field goals per game', 'data': 17 },
+  //     { 'name': 'three points per game', 'data': 9 },
+  //     { 'name': 'two points per game', 'data': 34 },
+  //     { 'name': 'rebounds per game', 'data': 5 },
+  //     { 'name': 'freethrows per game', 'data': 9 },
+  //   ];
+  //   const w = 700;
+  //   const h = 500;
+  //   const node = select(this.node)
+  //     .append('svg')
+  //     .attr('class', 'chart')
+  //     .attr('width', w)
+  //     .attr('height', h);
+  //   // const bounds = node.node().getBoundingClientRect();
+  //   const xScale = scaleBand();
+  //   xScale.domain(data.map(d => d.name));
+  //   xScale.padding(0.3);
+  //   xScale.range([0, w]);
 
-    chart.selectAll('.bar')
-      .data(data)
-      .enter()
-      .append('rect')
-      .classed('bar', true) // add class
-      .attr('x', d => xScale(d.name))
-      .attr('y', d => yScale(d.data))
-      .attr('height', d => (height - yScale(d.value)))
-      .attr('width', d => xScale.bandwidth())
-      .style('fill', (d, i) => colorScale(i));
-    chart.selectAll('.bar-label')
-      .data(data)
-      .enter()
-      .append('text')
-      .classed('bar-label', true)
-      .attr('x', d=>xScale(d.name) + xScale.bandwidth()/2)
-      .attr('dx', 0)
-      .attr('y', d => yScale(d.data))
-      .attr('dy', -6)  // explore with dx dy for text
-      .text(d => d.data)
-    const xAxis = d3.axisBottom()
-      .scale(xScale);
-    chart.append('g')
-      .classed('x axis', true)
-      .attr('transform', )
-    // const upd = node.selectAll('rect').data(data);
-    // upd.enter()
-    //     .append('rect')
-    //     .attr('x', d => xscale(d.name))
-    //     .attr('y', d => h -   (d.data))
-    //     .attr('width', xscale.bandwidth())
-    //     .attr('height', d => yscale(d.data))
-    //     .attr('fill', 'black');
-  }
-
-  drawChart(){
-    const data = [ 
-      { 'name': 'assists per game', 'data': 2 },
-      { 'name': 'blocks per game', 'data': 3.4 },
-      { 'name': 'field goals per game', 'data': 17 },
-      { 'name': 'three points per game', 'data': 9 },
-      { 'name': 'two points per game', 'data': 34 },
-      { 'name': 'rebounds per game', 'data': 5 },
-      { 'name': 'freethrows per game', 'data': 9 },
-    ];
-    const w = 800;
-    const h = 450;
-    const node = select(this.node)
-      .append('svg')
-      .attr('class', 'chart')
-      .attr('width', w)
-      .attr('height', h);
-    // const bounds = node.node().getBoundingClientRect();
-    
-  };
-  componentDidUpdate() {
-    this.drawChart();
-  }
-
-
-
-
-
-
-
-    // const data = arrtest;
-    // const w = 700;
-    // const h = 500;
-    // const svg = d3.select("body")
-    // .append("svg")
-    // .attr("width", w)
-    // .attr("height", h)
-    // .style("margin-left", 100);
-                  
-    // svg.selectAll("rect")
-    //   .data(data)
-    //   .enter()
-    //   .append("rect")
-    //   .attr("x", (d, i) => i * 70)
-    //   .attr("y", (d, i) => h - 10 * d.data)
-    //   .attr("width", 65)
-    //   .attr("height", (d, i) => d.data * 10)
-    //   .attr("fill", "green")
+  //   const yScale = scaleLinear();
+  //   yScale.domain([0, 100]);
+  //   yScale.range([0, h]);
+  //   const upd = node.selectAll('rect').data(data);
+  //   upd.enter()
+  //       .append('rect')
+  //       .attr('x', d => xScale(d.name))
+  //       .attr('y', d => h - yScale(d.data))
+  //       .attr('width', xScale.bandwidth())
+  //       .attr('height', d => yScale(d.data))
+  //       .attr('fill', 'black');
+  //   upd.append("g")
+  //       .attr("class", "x axis")
+  //       .attr("transform", "translate(0," + h + ")")
+  //       .call(xAxis);
+  //   const xAxis = node.axisBottom()
+  //     .scale(xScale)
+  //   const yAxis = node.axisLeft()
+  //     .scale(yScale);
+  //   upd.append("g")
+  //     .attr("class", "y axis")
+  //     .call(yAxis)
+  //     .append("text")
+  //     .attr("transform", "rotate(-90)")
+  //     .attr("y", 6)
+  //     .attr("dy", ".71em")
+  //     .style("text-anchor", "end")
+  //     .text("Frequency");
+  // };
+  // componentDidUpdate() {
+  //   this.drawChart();
   // };
 
-  // ComparisonRadial.propTypes = {
-  //   playerOne: PropTypes.object,
-  //   playerTwo: PropTypes.object,
-  // };      
   render(){
-    // <div id={"#" + root}></div>
     return (
-      <svg className = {styles.chart}
-        ref={node => {
-          this.node = node;
-        }}
-        ></svg>
+      <div>
+        <Chart />
+      </div>
     );
   }
 };
+//   render(){
+//     return (
+//       <svg className = {styles.chart}
+//         ref={node => {
+//           this.node = node;
+//         }}
+//         ></svg>
+//     );
+//   }
+// };
 
 
 export default ComparisonRadial;
