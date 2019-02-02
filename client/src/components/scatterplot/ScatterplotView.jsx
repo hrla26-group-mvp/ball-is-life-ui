a353b00dbf1f032edf38f0e186046aa3c4655e32
Martin Han <martinhan@Martins-MacBook-Pro-2.local>
2019-02-01 17:18:20 -0800

    [prog] Work on filters and styling

--- client/src/components/scatterplot/ScatterplotView.jsx
+++ client/src/components/scatterplot/ScatterplotView.jsx
@@ -10,17 +10,24 @@ export class ScatterplotView extends Component {
   }
 
   render = () => {
-    const { scatterplotData, playerData, xStat, yStat, updateScatterplotData } = this.props;
-    const stats = Object.keys(this.props.scatterplotData[0])
+    const { scatterplotData, playerData, xStat, yStat, updateScatterplotData, updateScatterplotXStat, updateScatterplotYStat } = this.props;
+    const stats = Object.keys(scatterplotData[0])
       .filter(field => !['id', 'api_id', 'full_name', 'first_name', 'last_name', 'position', 'primary_position'].includes(field))
       .map(stat => stat.split('_').map(word => word[0].toUpperCase() + word.slice(1)).join(' '));
-    
+    const positions = [...new Set(playerData.map(p => p.primary_position))];
+    const teams = [...new Set(playerData.map(p => p.team))];
+
     return (
       <div className={styles.scatterplotview} ref={this.ref}>
         <ScatterplotFiltersList
           stats={stats}
-          updateXStat={this.props.updateScatterplotXStat}
-          updateYStat={this.props.updateScatterplotYStat}
+          positions={positions}
+          teams={teams}
+          updateXStat={updateScatterplotXStat}
+          updateYStat={updateScatterplotYStat}
+          updateScatterplotData={updateScatterplotData}
+          scatterplotData={scatterplotData}
+          playerData={playerData}
         />
         <Scatterplot
           scatterplotData={scatterplotData}
